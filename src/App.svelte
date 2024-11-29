fe8b310ff1d719e590bd5f1c9b2f33511445424c
Davide Cristoni <davide@cristoni.it>
2024-11-29 22:29:44 +0100

    lezione 12

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,17 @@
   
   import Modal from './Modal.svelte';
 
+  let showModal = false;
+
   let people= [
     {name: 'Yoshi', beltColour: 'black', age: 25, id:1},
     {name: 'Mario', beltColour: 'orange', age: 35, id:2},
     {name: 'Luigi', beltColour: 'brown', age: 20, id:3}
   ];
+
+  const openModal = () => {
+    showModal = !showModal;
+  }
 
   const handleClick = (id) => {
     //delete person from list
@@ -15,8 +21,9 @@
 
 </script>
 
-<Modal />
+<Modal {showModal} message={"Hello, I am a prop value!"} on:click={openModal}/> <!-- showModal is a prop value and it is a shorthand, we do not have to specify the name if it is the same as the name of the variable -->
 <main>
+  <button on:click={openModal}>Open modal</button>
   {#each people as person (person.id)} <!-- person.id link the elements nder the hood with the specific element -->
     <div>
       <h4>{person.name}</h4>
--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,14 +1,39 @@
 <script>
- let showModal = true;
+    export let message = 'Hey, it\'s lonely in there!';
+    export let showModal = false;
+    export let isPromo = false;
 </script>
 
 {#if showModal}
-    <div class="backdrop">
+    <div class="backdrop" class:promo={isPromo} on:click >  <!-- class:promo adds promo class if isPromo is true -->
         <div class="modal">
-            <p>Sign up for offers!</p>
+            <p>{message}</p>
         </div>
     </div>
 {/if}
-<style>
 
+<main>
+</main>
+
+<style>
+    .backdrop {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background: rgba(0,0,0,0.8);
+    }
+
+    .modal {
+        padding: 10px;
+        border-radius: 10px;
+        max-width: 400px;
+        margin: 10% auto;
+        text-align: center;
+        background: white;
+    }
+
+    .promo .modal {
+        background: crimson;
+        color: white;
+    }
 </style>
